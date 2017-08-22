b9e59bb6f5587d3ce743eefd2dd7279fb8e75c6b
kameshmani <31080511+kameshmani@users.noreply.github.com>
2017-08-22 10:28:33 -0400

    Add files via upload

---
+++ nodeConsumeRequest.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.use(bodyParser.json());     // to support JSON-encoded bodies
+
+app.get('/processusers', (req, res) => {
+
+    var reqURL = "http://localhost:3001/users";
+    request(reqURL, function (err, response, body) {
+
+        log("body:" + JSON.stringify(body));
+
+        if (err) {
+            res.status(500);
+            res.header("Content-Type", "application/json");
+            res.send(buildErrorJson('ERROR', 'Error in processing request'));
+        } else if (!err && response.statusCode == 200) {
+            res.status(200);
+            res.header("Content-Type", "application/json");
+            res.send(body);
+        }
+    });
+});
+
+
+app.post('/processusers', (req, res) => {
+
+    var postData = {
+        userid: 101,
+        firstName: "Kamesh",
+        lastName: "TEST",
+        location: "NC"
+    };
+
+    var reqURL = 'http://localhost:3001/users'
+    var options = {
+        method: 'post',
+        body: postData,
+        json: true,
+        url: reqURL
+    }
+    request(options, function (err, response, body) {
+        if (err) {
+            res.status(500);
+            res.header("Content-Type", "application/json");
+            res.send(buildErrorJson('ERROR', 'Error in processing request'));
+        }
+        var bodyJson = JSON.stringify(body);
+        log("bodyJson:"+bodyJson);
+        // log("UserId:"+bodyJson.userid);
+        // log("firstName:"+bodyJson.firstName);
+        // log("lastName:"+bodyJson.lastName);
+        // log("location:"+bodyJson.location);
+        res.send(body);
+    })
+
+});
+app.listen(3000);
+
+var log = msg => console.log(msg);
+
+var buildErrorJson = (code, message) => {
+
+    var json = JSON.stringify(
+        {
+            "code": code,
+            "message": message
+        }
+    );
+    return json;
+}
---
+++ nodeWithOutBodyParser.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+app.use(function (req, res, next) {
+    var data = "";
+    console.log("Request Method:" + req.method)
+    if (req.method === "POST") {
+
+        req.on('data', function (chunk) {
+            data += chunk;
+            console.log("chunk:" + chunk);
+        })
+        req.on('end', function () {
+            req.rawBody = data;
+            req.jsonBody = JSON.parse(data);
+            next();
+        })
+    } else {
+        next();
+    }
+
+})
+app.post('/users', (req, res) => {
+    // console.log('Body JSON Data :' + JSON.stringify(req.jsonBody));
+    console.log("Post users");
+    var resData = {
+        "userid": req.jsonBody.userid,
+        "firstName": req.jsonBody.firstName,
+        "lastName": req.jsonBody.lastName,
+        "location": req.jsonBody.location
+    };
+
+    var usersFile = "users.json";
+    var usersJson = fs.readFileSync(usersFile);
+    var users = JSON.parse(usersJson);
+    users.push(resData);
+
+    fs.writeFileSync(usersFile, JSON.stringify(users));
+    res.header("Content-Type", "application/json");
+    res.send(users);
+});
+
+app.get('/users', (req, res) => {
+    console.log("get users");
+    var usersFile = "users.json";
+    var usersJson = fs.readFileSync(usersFile);
+    res.header("Content-Type", "application/json");
+
+    res.send(JSON.parse(usersJson));
+});
+
+
+app.listen(3001);
+
+
+//  {
+//         "userid": 104,
+//         "firstName": "Kamesh",
+//         "lastName": "TEST",
+//         "location": "NC"
+//     }
+
